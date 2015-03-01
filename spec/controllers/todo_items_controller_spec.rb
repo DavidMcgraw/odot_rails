95a84f0618004acb2ece92aca5e2048f7773c2e8
DavidMcgraw <mcgraw.percussion@gmail.com>
2015-03-01 03:22:06 -0500

    Add specs and code for editing todo items

--- spec/controllers/todo_items_controller_spec.rb
+++ spec/controllers/todo_items_controller_spec.rb
@@ -2,13 +2,4 @@ require 'spec_helper'
 
 describe TodoItemsController do
 
-  let!(:todo_list) { TodoList.create(title: "My title", description: "My list description") }
-
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index', todo_list_id: todo_list.id
-      response.should be_success
-    end
-  end
-
 end
